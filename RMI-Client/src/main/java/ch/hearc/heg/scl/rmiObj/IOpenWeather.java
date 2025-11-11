cd25c6d5d65efbaf1d49709c10d3f65b93bd7071
Matthieu Wehrli <matthieu.wehrli@he-arc.ch>
2025-11-11 18:11:19 +0100

    get weather logic

--- RMI-Client/src/main/java/ch/hearc/heg/scl/rmiObj/IOpenWeather.java
+++ RMI-Client/src/main/java/ch/hearc/heg/scl/rmiObj/IOpenWeather.java
@@ -3,8 +3,8 @@ package ch.hearc.heg.scl.rmiObj;
 import java.util.List;
 
 public interface IOpenWeather {
-    void setLatitude(double latitude);
-    void setLongitude(double longitude);
+    void setLatitude(Double latitude);
+    void setLongitude(Double longitude);
     String getWeather();
     List<String> getStations();
 }
--- RMI-Server/src/main/java/ch/hearc/heg/scl/rmiObj/OpenWeather.java
+++ RMI-Server/src/main/java/ch/hearc/heg/scl/rmiObj/OpenWeather.java
@@ -1,19 +1,37 @@
 package ch.hearc.heg.scl.rmiObj;
 
+import ch.hearc.ig.business.WeatherStation;
+import ch.hearc.ig.service.OWMManager;
+
 import java.util.List;
 
 public class OpenWeather implements IOpenWeather{
-    private double latitude;
-    private double longitude;
+    private Double latitude;
+    private Double longitude;
+    private static OWMManager manager;
 
-    public void setLatitude(double latitude) {
+    public void setLatitude(Double latitude) {
         this.latitude = latitude;
     }
 
-    public void setLongitude(double longitude) {
+    public void setLongitude(Double longitude) {
         this.longitude = longitude;
     }
     public String getWeather() {
+        String response;
+        if (this.latitude == null || this.longitude == null){
+            response = "Vous devez d'abord définir des coordonnées GPS pour obtenir des données météo";
+        } else {
+            if (this.manager == null) {
+                manager = new OWMManager();
+            }
+            try {
+                WeatherStation station = manager.call(this.latitude, this.longitude);
+                // ...
+            } catch (Exception e){
+                e.printStackTrace();
+            }
+        }
         return "";
     }
     @Override
